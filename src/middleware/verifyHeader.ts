5abf6397ddb34607576ecaad4533f274fd87b9c4
kmiyauchi <koji@supermojo.com>
2025-07-07 15:15:37 -0400

    Basic logic to charge token per request

--- src/middleware/verifyHeader.ts
+++ src/middleware/verifyHeader.ts
@@ -11,6 +11,10 @@ const JWT_SSI = process.env.OFFICIAL_SKYFIRE_EXPECTED_SSI!;
 
 const JWKS = createRemoteJWKSet(new URL(JWKS_URL));
 
+export interface SkyfireRequest extends IdentifyBotRequest {
+  jwtPayload?: string;
+}
+
 async function verifyHeader(
   req: IdentifyBotRequest,
   res: Response,
@@ -25,7 +29,7 @@ async function verifyHeader(
   const skyfireToken = req.header("skyfire-pay-id") || "";
 
   if (!skyfireToken) {
-    res.status(401).json({ error: "Missing Skyfire token" });
+    res.status(401).json({ error: "Missing Skyfire token `skyfire-pay-id`" });
     return;
   }
 
@@ -37,13 +41,18 @@ async function verifyHeader(
       algorithms: [JWT_ALGORITHM],
     });
 
+    console.log(payload, "JWT Payload");
+
     if ((payload as any).ssi !== JWT_SSI) {
       res.status(401).json({ error: "Invalid SSI in token" });
       return;
     }
 
     // Attach decoded info to req for downstream middleware
-    req.skyfireUser = payload;
+    console.log(payload, "JWT Payload");
+
+    (req as SkyfireRequest).jwtPayload = skyfireToken;
+
     next();
     return;
   } catch (err: unknown) {
